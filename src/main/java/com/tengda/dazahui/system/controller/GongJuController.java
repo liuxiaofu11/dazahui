eb9652b88528ff41da05549f07c2f9bec62fa7c0
11111 <1248827677@qq.com>
2021-02-05 15:43:31 +0800

    11111111111111111111

---
+++ src/main/java/com/tengda/dazahui/system/controller/GongJuController.java
@@ -0,0 +1,19 @@
+package com.tengda.dazahui.system.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author teswell
+ * @Date 2021/1/25 9:32
+ * @function
+ */
+public class GongJuController {
+
+
+    public static void main(String[] args) {
+
+        Integer a= 0x15;
+        System.out.println(a);
+    }
+}
